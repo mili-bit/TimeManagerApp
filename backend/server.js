6450edca0ae4074100c0c87b15c52ec28620334b
mili-bit <82903924+mili-bit@users.noreply.github.com>
2022-08-08 01:04:38 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const colors = require('colors');
-const dotenv = require('dotenv').config()
-const {errorHandler} = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')
+const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-app.use('/api/habits', require('./routes/habitRoutes'))
-app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/habits', require('./routes/habitRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 // Serve frontend
 
@@ -25,6 +25,6 @@ app.get('*', (req, res) =>
   )
 );
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
